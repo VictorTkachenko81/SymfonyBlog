a075b61439f2623d8c621f0e2a778965dc00ade6
Victor Tkachenko <tkachenko.victor.ukr@gmail.com>
2016-01-23 15:04:22 +0200

    fixed install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 #Documentation:
 #http://www.freeos.com/guides/lsst/
 #http://linuxconfig.org/bash-scripting-tutorial
+#http://www.freeos.com/guides/lsst/misc.htm#colorfunandmore
 
-echo -e "\nWelcome to install script!\n"
+echo -e "\n\033[1m\033[34mWelcome to install script!\033[0m\n"
 echo "Choose what you want to do:"
 echo " 1 - composer self-update and install"
 echo " 2 - npm, bower install"
@@ -13,84 +14,72 @@ echo " 5 - load fixtures (dev|test|default:dev)"
 echo " 6 - clear cache (prod|dev|test|all|default:all)"
 echo " 9 - run all command step by step"
 echo " 0 - exit"
-echo -n "Please choose > "
+echo -e -n "\n\033[1m\033[34m Please choose > \033[0m"
 
 function composer_install
 {
-    echo "Composer install"
+    echo -e "\n\033[1m\033[34m Composer install \033[0m"
 	composer self-update
 	composer install -n
 }
 
 function npm_bower_install
 {
-	echo "Npm, bower install"
+	echo -e "\n\033[1m\033[34m Npm, bower install \033[0m"
 	npm install
 	./node_modules/.bin/bower install
 }
 
 function gulp_run
 {
-	echo "Gulp run"
+	echo -e "\n\033[1m\033[34m Gulp run \033[0m"
 	./node_modules/.bin/gulp
 }
 
 function database_update
 {
-	echo "Updating database"
+	echo -e "\n\033[1m\033[34m Updating database \033[0m"
 	app/console doctrine:schema:drop --force
 	app/console doctrine:schema:update --force
 }
 
 function load_fixtures
 {
-	echo "Loading fixtures"
+	echo -e "\n\033[1m\033[34m Loading fixtures \033[0m"
 
-    if $input_argument ; then
-        local input_argument='dev'
-    fi
-
-	case $input_argument in
-        dev ) echo "Used - dev"
-            app/console doctrine:fixtures:load -n
-            ;;
-        test ) echo "Used - test"
+	case ${input_argument} in
+        test ) echo -e " Used -\033[1m\033[32m test \033[0m"
             app/console doctrine:fixtures:load -n -e test
             ;;
-        exit ) echo "Good bye!"
+        * ) echo -e " Used -\033[1m\033[32m dev \033[0m"
+            app/console doctrine:fixtures:load -n
     esac
 }
 
 function clear_cache
 {
-	echo "Clearing caches"
+	echo -e "\n\033[1m\033[34m Clearing caches \033[0m"
 
-    if $input_argument ; then
-        local input_argument='all'
-    fi
-
-	case $input_argument in
-        prod ) echo "Used - prod"
+	case ${input_argument} in
+        prod ) echo -e " Used -\033[1m\033[32m  prod \033[0m"
             app/console cache:clear -e prod
             ;;
-        dev ) echo "Used - dev"
+        dev ) echo -e " Used -\033[1m\033[32m  dev \033[0m"
             app/console cache:clear -e dev
             ;;
-        test ) echo "Used - test"
+        test ) echo -e " Used -\033[1m\033[32m  test \033[0m"
             app/console cache:clear -e test
             ;;
-        all ) echo "Used - all"
+        * ) echo -e " Used -\033[1m\033[32m all \033[0m"
             app/console cache:clear -e prod
             app/console cache:clear -e dev
             app/console cache:clear -e test
-            ;;
-        exit ) echo "Good bye!"
     esac
 }
 
 function all_run
 {
-	echo "Run All scripts"
+	echo -e "\n\033[1m\033[34m Run All scripts \033[0m"
 	composer_install
 	npm_bower_install
 	gulp_run
@@ -99,11 +88,10 @@ function all_run
 	clear_cache
 }
 
-
 read input_command input_argument
-echo $input_command $input_argument
+#echo ${input_command} ${input_argument}
 
-case $input_command in
+case ${input_command} in
     1 ) composer_install
         ;;
     2 ) npm_bower_install
@@ -118,5 +106,5 @@ case $input_command in
 		;;
     9 ) all_run
 		;;
-    0 ) echo "Good bye!"
+    * ) echo -e "\n\033[1m\033[34m Good bye! \033[0m"
 esac
